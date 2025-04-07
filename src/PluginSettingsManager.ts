b209367f51b767efd45d5822f4043016040815f5
Michael Naumov <mnaoumov@gmail.com>
2025-04-07 02:11:57 -0600

    Update template

--- src/PluginSettingsManager.ts
+++ src/PluginSettingsManager.ts
@@ -7,15 +7,16 @@ import type { PluginTypes } from './PluginTypes.ts';
 import { PluginSettings } from './PluginSettings.ts';
 
 export class PluginSettingsManager extends PluginSettingsManagerBase<PluginTypes> {
-  protected override addValidators(): void {
-    this.addValidator('textSetting', (value): MaybeReturn<string> => {
+  protected override createDefaultSettings(): PluginSettings {
+    return new PluginSettings();
+  }
+
+  protected override registerValidators(): void {
+    super.registerValidators();
+    this.registerValidator('textSetting', (value): MaybeReturn<string> => {
       if (value === 'foo') {
         return 'Foo is not allowed';
       }
     });
-  }
-
-  protected override createDefaultSettings(): PluginSettings {
-    return new PluginSettings();
   }
 }
