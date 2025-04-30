4f00a09ca87a9e901cf345c3236593523f1c9000
Michael Naumov <mnaoumov@gmail.com>
2025-04-30 13:13:31 -0600

    Add comment

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import './styles/main.scss';
+import './styles/main.scss'; // Do not delete this line if you want to have a styles.css file in your build output.
 import { Plugin } from './Plugin.ts';
 
 // eslint-disable-next-line import-x/no-default-export
